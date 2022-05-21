0bc74334c1556520bfc791641772b21a7fa144a6
Nelson1411 <drosswerever@gmail.com>
2022-05-20 23:09:21 -0400

    feat: add echo-hacks

---
+++ echo/hack1.js
@@ -0,0 +1,5 @@
+let array = [100,200,300,400,500,600,700];
+
+array.splice(0);
+
+console.log(array);
---
+++ echo/hack7.js
@@ -0,0 +1,5 @@
+let array = [100,200,300,400,500,600,700];
+
+array.splice(2,1,"echo");
+
+console.log(array);
